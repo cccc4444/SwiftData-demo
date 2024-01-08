965ebd3533d8dbe10322909e7ab82260e0498569
dankus <30757557+cccc4444@users.noreply.github.com>
2024-01-08 15:24:42 +0200

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Danylo Kushlianskyi on 08.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
